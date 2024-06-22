4c9a03732bf219662fa257be475955f54e9e3884
ripa01 <ripabiswas2739@gmail.com>
2024-06-23 00:23:48 +0600

    Design the home page components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 // import Dashboard from './components/Dashboard';
-import LanLonBoard from './components/LanLonBoard';
+import LanLonBoard from "./components/LanLonBoard";
+import Country from "./components/Country";
+import Home from "./components/Home";
+
+import { BrowserRouter,Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      {/* <Dashboard/> */}
-      <LanLonBoard />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <Home />
+              </>
+            }
+          />
+          <Route exact path="/LanLonBoard" element={<LanLonBoard />} />
+          <Route exact path="/Country" element={<Country />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
